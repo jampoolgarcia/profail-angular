b907e76c33f9ae5c381bf50ca723f66c9b148bd4
jampool garcia <jampoolgarcia@gmail.com>
2022-12-26 10:54:12 -0300

    getting data info-pagena.service

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,10 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pagina';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  constructor(private _http: HttpClient) { }
+  info!: InfoPagina;
+  cargar = false;
+
+  constructor(private _http: HttpClient) {
+
+    // Leer el json
+    this._http.get('assets/data/data-pagina.json')
+      .subscribe( (res: InfoPagina) => {
+        this.cargar = true;
+        this.info = res;
+    });
+
+  }
+
+
+   
+
+  
 }
